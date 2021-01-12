d54319aa94f7bacdac76e606cb88668ce2d9fd60
lisa0015 <lisasa0215@gmail.com>
2021-01-12 14:06:41 -0800

    gjafh

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-'use strict'; 
+// 'use strict'; 
 
-let hasDirversLicense = false;
-const passTest =true;
-if(passTest)hasDirverLicense=true;
-if(hasDirversLicense) console.log('I can dirve :D');
+// let hasDirversLicense = false;
+// const passTest =true;
+// if(passTest)hasDirverLicense=true;
+// if(hasDirversLicense) console.log('I can dirve :D');
+
+
+// 33.functions
+
+// function logger(){
+//       console.log('My name is Lisa.');
+// }
+// logger();
+// logger();
+// logger();
+function fruitProcessor(apples, oranges){
+      const juice =`Juice with ${apples} apples and ${oranges} orange.`;
+      return juice;
+}
+const appleJuice=fruitProcessor(3,2);
+console.log(appleJuice);

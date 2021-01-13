d4d7103095dc76ce9e216fea41567cac0be7cba9
lisa0015 <lisasa0215@gmail.com>
2021-01-13 15:40:04 -0800

    first

--- script.js
+++ script.js
@@ -14,9 +14,32 @@
 // logger();
 // logger();
 // logger();
-function fruitProcessor(apples, oranges){
-      const juice =`Juice with ${apples} apples and ${oranges} orange.`;
-      return juice;
+// function fruitProcessor(apples, oranges){
+//       const juice =`Juice with ${apples} apples and ${oranges} orange.`;
+//       return juice;
+// }
+// const appleJuice=fruitProcessor(3,2);
+// console.log(appleJuice);
+
+
+//34.
+// function calcAge1(birthYear){
+//     return 2037-birthYear;
+// }
+// const age1=calcAge1(1988);
+// console.log(age1);
+
+// const calcAge2=function(birthYear){
+//       return 2037-birthYear;
+// }
+// const age2=calcAge2(1992);
+// console.log(age1,age2);
+//arrow function
+//function expression
+const calcAge2 =function (birthYear){
+      return 2037-birthYear;
 }
-const appleJuice=fruitProcessor(3,2);
-console.log(appleJuice);
+//arrow function
+const calcAge3 = birthYear => 2037-birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
